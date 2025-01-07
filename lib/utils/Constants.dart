6fa1a48ba26305bd2e9bfc2938ec4c6a7cf37fc5
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-07 18:23:26 +0530

    Create Constants.dart

---
+++ lib/utils/Constants.dart
@@ -0,0 +1,11 @@
+class Constants  {
+
+  static const String BASE_URL = "http://192.168.1.211:8000/api";
+
+  static const String ALL_TRANSACTION_ROUTE = "/transactions";
+
+  static const String INCOME_TRANSACTION_ROUTE = "/transactions?type=income";
+
+  static const String EXPENSE_TRANSACTION_ROUTE = "/transactions?type=expense";
+
+}
