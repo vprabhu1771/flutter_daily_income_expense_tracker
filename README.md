cd7b9c09549446fd6fddbe9301c99a1a6827e4bb
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-07 18:22:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_daily_income_expense_tracker
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_daily_income_expense_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_daily_income_expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
