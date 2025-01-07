9e2601e7feef138175a3fc5d4897019c09d60be2
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-07 19:14:33 +0530

    IncomeTrasactionScreen API logic added

--- lib/screens/IncomeTrasactionScreen.dart
+++ lib/screens/IncomeTrasactionScreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../models/Transaction.dart';
+import '../utils/Constants.dart';
 
 class IncomeTrasactionScreen extends StatefulWidget {
 
@@ -11,15 +16,92 @@ class IncomeTrasactionScreen extends StatefulWidget {
 }
 
 class _IncomeTrasactionScreenState extends State<IncomeTrasactionScreen> {
+
+  List<Transaction> transactions = [];
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTransactions();
+  }
+
+  Future<void> fetchTransactions() async {
+    try {
+      final response = await http
+          .get(Uri.parse(Constants.BASE_URL + Constants.INCOME_TRANSACTION_ROUTE));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body)['data'];
+
+        setState(() {
+          transactions = data.map((row) => Transaction.fromJson(row)).toList();
+          isLoading = false;
+        });
+      } else {
+        showError('Failed to load transactions.');
+        throw Exception('Failed to load transactions. ' +
+            Constants.BASE_URL +
+            Constants.INCOME_TRANSACTION_ROUTE);
+      }
+    } catch (e) {
+      showError('An error occurred while fetching transactions: $e');
+      print('Error: $e');
+    } finally {
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  void showError(String message) {
+    if (mounted) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(message)));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(widget.title),
-      ),
+      body: isLoading
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : transactions.isEmpty
+          ? Center(child: Text('No transactions found.'))
+          : ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (context, index) {
+            final row = transactions[index];
+            // print(row);
+
+            return Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 8.0, horizontal: 16.0),
+                child: ListTile(
+                  leading: Icon(
+                    row.type == 'INCOME'
+                        ? Icons.arrow_downward
+                        : Icons.arrow_upward,
+                    color: row.type == 'INCOME'
+                        ? Colors.green
+                        : Colors.red,
+                  ),
+                  title: Text(
+                    '${row.type}: \$${row.amount}',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(row.description),
+                  trailing: Text(row.date),
+                ));
+          }),
     );
   }
 }
