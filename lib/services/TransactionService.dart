65dd8b3c76c2821cbc0010dc3b12bf8826e40d1f
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-09 23:14:51 +0530

    getTransaction, updateTransaction service logic added

--- lib/services/TransactionService.dart
+++ lib/services/TransactionService.dart
@@ -7,7 +7,7 @@ class TransactionService {
 
   Future<void> createTransaction({
     required String type,
-    required double amount,
+    required String? amount,
     required String description,
     required String date,
   }) async {
@@ -33,6 +33,63 @@ class TransactionService {
         // Error handling
         print("Failed to create transaction: ${response.body}");
         print(Constants.BASE_URL + Constants.ADD_TRANSACTION);
+      }
+    } catch (error) {
+      print("Error occurred: $error");
+    }
+  }
+
+  // Fetch a single transaction by ID
+  Future<Map<String, dynamic>?> getTransaction(String transactionId) async {
+    final url = Uri.parse(
+        Constants.BASE_URL + Constants.SINGLE_TRANSACTION_ROUTE.replaceFirst('{id}', transactionId));
+    final headers = {'Content-Type': 'application/json'};
+
+    try {
+      final response = await http.get(url, headers: headers);
+
+      if (response.statusCode == 200) {
+        // Success
+        final data = jsonDecode(response.body);
+        print("Transaction fetched successfully: $data");
+        return data['data']; // Assuming the response has a 'data' key
+      } else {
+        // Error handling
+        print("Failed to fetch transaction: ${response.body}");
+        return null;
+      }
+    } catch (error) {
+      print("Error occurred: $error");
+      return null;
+    }
+  }
+
+  Future<void> updateTransaction({
+    required String id,
+    required String type,
+    required String amount,
+    required String description,
+    required String date,
+  }) async {
+    final url = Uri.parse(
+      Constants.BASE_URL + Constants.UPDATE_TRANSACTION_ROUTE.replaceFirst("{id}", id),
+    );
+    final headers = {'Content-Type': 'application/json'};
+
+    final body = jsonEncode({
+      'type': type,
+      'amount': amount,
+      'description': description,
+      'date': date,
+    });
+
+    try {
+      final response = await http.put(url, headers: headers, body: body);
+
+      if (response.statusCode == 200) {
+        print("Transaction updated successfully");
+      } else {
+        print("Failed to update transaction: ${response.body}");
       }
     } catch (error) {
       print("Error occurred: $error");
